db3e3cbfa0fa6650b38b321668e04f8c9324d200
Brian Penner <brian@planetnine.tech>
2021-10-02 16:02:23 -0500

    first commit

---
+++ plugin-notes.php
@@ -0,0 +1,24 @@
+<?php 
+/**
+* Plugin Name:     plugin-notes
+* Plugin URI:      https://www.planetnine.tech/
+* Description:     
+* Author:          Brian Penner
+* Author URI:      https://www.planetnine.tech/
+* Text Domain:     plugin-notes
+* Domain Path:     /languages
+* Version:          0.1.0
+*
+* @package plugin-notes
+*/
+
+class pluginNotes {
+public function hooks() {}
+    /* 
+    * 
+    */
+    public function __construct() {
+        
+    }
+
+}
