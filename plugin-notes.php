01f63e2e248875df83b7bab1e56aba30e05a981b
Brian Penner <brian@planetnine.tech>
2021-10-02 16:52:06 -0500

    added options page

--- plugin-notes.php
+++ plugin-notes.php
@@ -1,24 +1,36 @@
-<?php 
-/**
-* Plugin Name:     plugin-notes
-* Plugin URI:      https://www.planetnine.tech/
-* Description:     
-* Author:          Brian Penner
-* Author URI:      https://www.planetnine.tech/
-* Text Domain:     plugin-notes
-* Domain Path:     /languages
-* Version:          0.1.0
-*
-* @package plugin-notes
-*/
+<?php
 
-class pluginNotes {
-public function hooks() {}
+/**
+ * Plugin Name:     Plugin Notes
+ * Plugin URI:      https://www.planetnine.tech/
+ * Description:     
+ * Author:          Brian Penner
+ * Author URI:      https://www.planetnine.tech/
+ * Text Domain:     plugin-notes
+ * Domain Path:     /languages
+ * Version:          0.1.0
+ *
+ * @package plugin-notes
+ */
+
+class pluginNotes
+{
+    public function hooks()
+    {
+    }
     /* 
     * 
     */
-    public function __construct() {
-        
+    public function __construct()
+    {
+        add_action('acf/init', [$this, 'addOptionsPage']);
     }
 
+    public function addOptionsPage()
+    {
+        acf_add_options_page([
+            'page_title' => 'Plugin Notes'
+        ]);
+    }
 }
+$pluginNotes = new PluginNotes();
